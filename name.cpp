3c16365e3abc1f8bfee4fc10b4f5f305f5a5efe1
DexinLi <ldx94@hotmail.com>
2017-08-25 18:31:52 +0800

    update

--- name.cpp
+++ name.cpp
@@ -6,18 +6,21 @@ struct A{
 };
 
 template<template <class> class T1, class T2>
-struct B
+struct B:A<T2>
 {
-    void foo(T1<T2> t){
+    void foo(T1<T1<T2>> t){
         return ;
     }
 };
 
+
 int main()
 {
+    auto g = [](auto a){return a;};
+    int arr[sizeof(int)];
     A<int> a;
     a.foo(a);
-    B<A,int> b;
-    b.foo(a);
+    // B<A,int> b;
+    // b.foo(a);
     return 0;
 }
