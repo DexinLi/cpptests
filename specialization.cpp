8e0843cd627f9f6a838d40464385133b53b88125
DexinLi <ldx94@hotmail.com>
2018-01-31 23:00:58 +0800

    update

--- specialization.cpp
+++ specialization.cpp
@@ -1,5 +1,8 @@
 template<class T>
 class A{};
 
-template<class T1,class T2>
-class A<T1(T2)> {};
+// template<class T1,class T2>
+// class A<T1(T2)> {};
+
+template<class T,class... Args>
+class A<T(Args...)>{};
